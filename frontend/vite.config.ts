408e18ab621b556ee4eccbf00ea51bc48c5f2d1e
AFCMS <afcm.contact@gmail.com>
2025-04-14 14:12:40 +0200

    Pin WebSocket port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,10 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
         strictPort: true,
+        hmr: {
+            protocol: "ws",
+            port: 5173,
+        }
     },
     plugins: [react()],
     assetsInclude: ["**/*.gltf"],
